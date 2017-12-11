aaafd095395aea19ba56a133e5fa5ddb321681c2
Brandon Middleton <brandon.middleton@gmail.com>
2017-12-10 23:24:37 -0800

    Update portfolio deployment function for Lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,7 +4,10 @@ import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+
+def lambda_handler(event, context):
+
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
 portfolio_bucket = s3.Bucket('brmiddle-portfoliobucket')
 build_bucket = s3.Bucket('brmiddle-portfoliobucket')
@@ -19,3 +22,7 @@ with zipfile.ZipFile(portfolio_zip) as myzip:
         portfolio_bucket.upload_fileobj(obj, nm,
             ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
         portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    print "Job Done!"
+
+    return 'Hello from Lambda'
